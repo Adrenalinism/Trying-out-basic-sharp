99fd6fd60ee4dce77acf1ee88fac7004c51a057a
Marko Bandzo <marko_bandzo@hotmail.com>
2018-04-09 10:26:31 +1200

    Add files via upload

--- copy2.cpp
+++ copy2.cpp
@@ -8,29 +8,10 @@
  * This software is provided "as is" without express or implied
  * warranty, and with no claim as to its suitability for any purpose.
  */
-#include "algostuff.hpp"
-using namespace std;
+#include <iostream>
 
-int main()
+int main ()
 {
-    /* initialize source collection with ``..........abcdef..........''
-     */
-    vector<char> source(10,'.');
-    for (int c='a'; c<='f'; c++) {
-        source.push_back(c);
-    }
-    source.insert(source.end(),10,'.');
-    PRINT_ELEMENTS(source,"source: ");
-
-    // copy all letters three elements in front of the 'a'
-    vector<char> c1(source.begin(),source.end());
-    copy (c1.begin()+10, c1.begin()+16,  // source range
-          c1.begin()+7);                 // destination range
-    PRINT_ELEMENTS(c1,"c1:     ");
-
-    // copy all letters three elements behind the 'f'
-    vector<char> c2(source.begin(),source.end());
-    copy_backward (c2.begin()+10, c2.begin()+16,  // source range
-                   c2.begin()+19);                // destination range
-    PRINT_ELEMENTS(c2,"c2:     ");
+    // copy all standard input to standard output
+    std::cin >> std::noskipws >> std::cout.rdbuf();
 }
